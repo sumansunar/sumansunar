4598d4f4990e16b7461e2169e04605aee724ba7e
sumansunar <sinaeve077@gamil.com>
2023-04-07 01:29:40 -0700

    new crud

--- form.js
+++ form.js
@@ -14,4 +14,30 @@ function showUserCreateBox() {
     });
   }
 
+
+  function showeditBox() {
+    Swal.fire({
+      title: "Edit",
+      html:
+        '<input id="id" type="hidden">' +
+        '<input id="name" class="swal2-input" placeholder="NAME">' +
+        '<input id="date" class="swal2-input" placeholder="DATE">' ,
+        // '<input id="username" class="swal2-input" placeholder="Username">' +
+        // '<input id="email" class="swal2-input" placeholder="Email">',
+    //   focusConfirm: false,
+    //   preConfirm: () => {
+    //     userCreate();
+    //   },
+    });
+  }
+
+
+  function deleteItem() {
+    Swal.fire({
+      title: "Delete",
+      html:
+        '<p>Are you sure!</p>'
+       
+    });
+  }
   
